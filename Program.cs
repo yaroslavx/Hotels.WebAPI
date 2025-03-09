2c30310f10320533fade3a1e7ae9aff77a3cc5c3
yaroslavx <yakupov.yaroslav@icloud.com>
2025-03-09 09:19:23 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var hotels = new List<Hotel>();
+
+app.MapGet("/hotels", () => hotels);
+app.MapGet("/hotels/{id}", (int id) => hotels.FirstOrDefault(h => h.Id == id));
+app.MapPost("/hotels", (Hotel hotel) => hotels.Add(hotel));
+app.MapPut("/hotels/{id}", (int id, Hotel hotel) => {
+    var index = hotels.FindIndex(h => h.Id == id);
+    if (index < 0) {
+        throw new Exception("Not found");
+    }
+    hotels[index] = hotel;
+});
+app.MapDelete("/hotels/{id}", (int id) => {
+    var index = hotels.FindIndex(h => h.Id == id);
+    if (index < 0) {
+        throw new Exception("Not found");
+    }
+    hotels.RemoveAt(index);
+});
+
+app.Run();
+
+public class Hotel 
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
+

1021cf1c72b74ad39171737d030ec0697a57a31e
yaroslavx <yakupov.yaroslav@icloud.com>
2025-03-09 09:20:56 +0300

    INIT

--- obj/Debug/net9.0/Hotels.WebAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/Hotels.WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotels.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c30310f10320533fade3a1e7ae9aff77a3cc5c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotels.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotels.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

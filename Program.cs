273fb426be2248288b60018815f705d7029c476b
yaroslavx <yakupov.yaroslav@icloud.com>
2025-03-09 18:52:03 +0300

    MAINTAIN: Added db

--- Program.cs
+++ Program.cs
@@ -1,27 +1,76 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<HotelDb>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("HotelDb"));
+});
+
 var app = builder.Build();
 
-var hotels = new List<Hotel>();
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<HotelDb>();
+    db.Database.EnsureCreated();
+}
 
-app.MapGet("/hotels", () => hotels);
-app.MapGet("/hotels/{id}", (int id) => hotels.FirstOrDefault(h => h.Id == id));
-app.MapPost("/hotels", (Hotel hotel) => hotels.Add(hotel));
-app.MapPut("/hotels/{id}", (int id, Hotel hotel) => {
-    var index = hotels.FindIndex(h => h.Id == id);
-    if (index < 0) {
-        throw new Exception("Not found");
-    }
-    hotels[index] = hotel;
+app.MapGet("/hotels", async (HotelDb db) => await db.Hotels.ToListAsync());
+
+app.MapGet("/hotels/{id}", async (int id, HotelDb db) => 
+        await db.Hotels.FirstOrDefaultAsync(h => h.Id == id) is Hotel hotel
+            ? Results.Ok(hotel)
+            : Results.NotFound());
+
+app.MapPost("/hotels", async ([FromBody] Hotel hotel, HotelDb db) =>
+{
+    db.Hotels.Add(hotel);
+    await db.SaveChangesAsync();
+    return Results.Created($"/hotels/{hotel.Id}", hotel);
 });
-app.MapDelete("/hotels/{id}", (int id) => {
-    var index = hotels.FindIndex(h => h.Id == id);
-    if (index < 0) {
-        throw new Exception("Not found");
+
+app.MapPut("/hotels/{id}", async (int id, [FromBody] Hotel hotel, HotelDb db) => {
+    var hotelFromDb = await db.Hotels.FirstOrDefaultAsync(h => h.Id == id);
+
+    if (hotelFromDb == null)
+    {
+        return Results.NotFound();
     }
-    hotels.RemoveAt(index);
+
+    hotelFromDb.Name = hotel.Name;
+    hotelFromDb.Latitude = hotel.Latitude;
+    hotelFromDb.Longitude = hotel.Longitude;
+    
+    await db.SaveChangesAsync();
+    
+    return Results.NoContent();
 });
+app.MapDelete("/hotels/{id}", async  (int id, HotelDb db) => {
+    var hotelFromDb = await db.Hotels.FirstOrDefaultAsync(h => h.Id == id);
+
+    if (hotelFromDb == null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Hotels.Remove(hotelFromDb);
+    
+    await db.SaveChangesAsync();
+    
+    return Results.NoContent();
+});
+
+app.UseHttpsRedirection();
 
 app.Run();
+
+public class HotelDb : DbContext
+{
+    public HotelDb(DbContextOptions<HotelDb> options) : base(options)
+    {
+    }
+
+    public DbSet<Hotel> Hotels => Set<Hotel>();
+}
 
 public class Hotel 
 {
--- obj/Debug/net9.0/Hotels.WebAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/Hotels.WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hotels.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1021cf1c72b74ad39171737d030ec0697a57a31e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffc9250f9c934f523d719dbeb8e0112acfe84058")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hotels.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hotels.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
